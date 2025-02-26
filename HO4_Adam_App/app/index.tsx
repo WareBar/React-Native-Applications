fdb0cc46892baec994be4fa7fd39688e52824de2
WareBar <barryadam615@gmail.com>
2025-02-26 21:16:27 +0800

    added initial dashboard

---
+++ HO4_Adam_App/app/index.tsx
@@ -0,0 +1,62 @@
+import { View, Text, Image, StyleSheet, Button } from "react-native";
+
+
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      {/* BELOW IS THE COMPONENT FOR TOP NAVIGATION */}
+      <View style={styles.headNav}>
+        <View style={styles.headNavRight}>
+          <Image resizeMode="cover" style={styles.profile} source={require('../assets/images/472875871_628437569639942_8004256870174663318_n.jpg')}/>
+          <View>
+            <Text>Hello</Text>
+            <Text style={styles.name}>Barry Adam</Text>
+          </View>
+        </View>
+        <View>
+          <Button title="Search"></Button>
+        </View>
+      </View>
+
+      {/* BELOW IS THE COMPONENT FOR CARD SHOW */}
+
+    </View>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%'
+  },
+  headNav:{
+    // borderWidth: 1,
+    flexDirection: 'row',
+    // : '10, 20',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingInline: 15
+  },
+
+  headNavRight:{
+    flexDirection: 'row',
+    marginTop: 10,
+    marginBottom: 10
+  },
+
+  profile:{
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+    marginRight: 10
+  },
+  name:{
+    fontSize: 16,
+    fontWeight: 'bold'
+  }
+})
+
+
+export default App;
